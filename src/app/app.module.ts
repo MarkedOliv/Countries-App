58b6b5bd840935a515302cf361ce0a6d708d4bd0
MarkedOliv <olivdev4@gmail.com>
2023-06-12 13:08:19 -0400

    push to github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule} from './shared/shared.module'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
