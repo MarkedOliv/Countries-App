efe6515b25d18c74901e48dfe27cc71e5edf9b6c
MarkedOliv <olivdev4@gmail.com>
2023-06-13 21:01:10 -0400

    Using lazy for countries module

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CountriesRoutingModule } from './countries-routing.module';
+
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionComponent } from './pages/by-region/by-region.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionComponent,
+    CountryPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CountriesRoutingModule
   ]
 })
 export class CountriesModule { }
